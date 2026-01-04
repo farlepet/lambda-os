ad3fc8254c28bbbd780fd3de0504a0d9dde67436
Peter Farley <pfarley@pfarley.dev>
2026-01-03 20:39:57 -0600

    Add FreeBSD support to build-wrapper.sh

--- build-wrapper.sh
+++ build-wrapper.sh
@@ -1,12 +1,44 @@
 #!/bin/sh
 
+os_name=`uname -s`
+
+RUN_ARGS=""
+
+if [ "$os_name" = "FreeBSD" ]; then
+    if ! which podman > /dev/null; then
+        echo "Must have Podman installed for FreeBSD!"
+        exit 1
+    fi
+    if ! which sudo > /dev/null; then
+        echo "Only 'sudo' is supported for priviledge escalation. This is required as Podman does not support rootless operation on FreeBSD."
+        exit 1
+    fi
+    COMMAND="sudo podman"
+    # We need to use Linuxulator on FreeBSD
+    RUN_ARGS="--os linux"
+else
+    if which docker > /dev/null; then
+        # Assume user has permissions
+        COMMAND="docker"
+    elif which podman > /dev/null; then
+        # Assume rootless is supported
+        COMMAND="podman"
+    else
+        echo "Must have either Docker or Podman installed!"
+        exit 1
+    fi
+fi
+
+
 TARGET=${LAMBDA_TARGET:-i686-elf}
 TAG=${LAMBDA_TAG:-latest}
 
-docker run -it --rm \
-           -v ${PWD}:/code:rw \
-           -w /code \
-           -u $(id -u):$(id -g) \
-           -e CROSS_COMPILE="/opt/lambda-cross/bin/$TARGET-" \
-           git.pfarley.dev/lambda-os/builder:$TAG-$TARGET \
-           "$@"
+$COMMAND run $RUN_ARGS \
+             -it --rm \
+             -v ${PWD}:/code:rw \
+             -w /code \
+             -u $(id -u):$(id -g) \
+             -e CROSS_COMPILE="/opt/lambda-cross/bin/$TARGET-" \
+             -e HOST_CC="gcc" \
+             git.pfarley.dev/lambda-os/builder:$TAG-$TARGET \
+             "$@"
